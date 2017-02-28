d7e354f74df1b73af950071aa0fe4e7aee12cd50
Usama Ahmed <maxpaynestory@gmail.com>
2017-02-28 20:36:23 +0100

    fixed the license header

--- src/IdentityAccess/Application/Service/RegisterUser.php
+++ src/IdentityAccess/Application/Service/RegisterUser.php
@@ -1,9 +1,7 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * For the full copyright and license information, please view the LICENSE.txt
  */
 
 namespace IdentityAccess\Application\Service;
